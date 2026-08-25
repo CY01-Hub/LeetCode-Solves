5cb206fbaa2c307cd9f74a79b8c80b9239a910ad
CY01-Hub <dhrubodey18@gmail.com>
2026-08-25 21:56:09 +0530

    Time: 4 ms (65.16%) | Memory: 20.1 MB (70.79%) - LeetSync

---
+++ 338-counting-bits/counting-bits.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        r = [0] * (n + 1)
+
+        for i in range(1, n + 1):
+            r[i] = r[i >> 1] + (i & 1)
+
+        return r
