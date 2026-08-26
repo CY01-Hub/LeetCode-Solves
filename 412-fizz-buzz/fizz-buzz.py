17ff3d111e819208584ae8d7301e2f1fc22293de
CY01-Hub <dhrubodey18@gmail.com>
2026-08-26 21:24:51 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (53.65%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.py
+++ 412-fizz-buzz/fizz-buzz.py
@@ -4,10 +4,10 @@ class Solution:
         for i in range(1, n+1):
             if i % 3 == 0 and i % 5 == 0:
                 ans.append("FizzBuzz")
-            elif i % 3 == 0 and i % 5 != 0:
+            elif i % 3 == 0:
                 ans.append("Fizz")
-            elif i % 3 != 0 and i % 5 == 0:
+            elif i % 5 == 0:
                 ans.append("Buzz")
             else:
-                ans.append(f"{i}")
+                ans.append(str(i))
         return ans
